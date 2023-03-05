784abe86d4b357050ee4e7402f98fbb7e47cb2a5
Jiaxin <114979508+Whyicantlogin@users.noreply.github.com>
2023-03-05 22:29:04 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -5,54 +5,54 @@ import sys
 import threading
 
 def compute_height(n, parents):
-    # izveido listu 
-    children = [[] for _ in range(n)]
-    for child, parent in enumerate(parents):
-        if parent != -1:
-            children[parent].append(child)
-    s = parents.index(-1) # inicializēt koku sakne
-    rinda = [(s,1)]
-    height = 0 
-    while rinda:
-        n, l = rinda.pop(0)
-        if l > height:
-            height = l
-        for child in children[n]:
-            rinda.append((child, l+1))
-
-    return height
+    # 
+    heights = {}
+    
+    
+    def get_height(node):
+        
+        if node in heights:
+            return heights[node]
+        
+        if all(parents[child] != node for child in range(n)):
+            heights[node] = 1
+        
+        else:
+            heights[node] = 1 + max(get_height(child) for child in range(n) if parents[child] == node)
+        return heights[node]
+    max_height = max(get_height(node) for node in range(n))
+    return max_height
 
 
 
 def main():
-    # implement input form keyboard and from files
     user = input("'I' for input, 'F' for file: ")
-    if user == "I":
+    if "I" in user:
         n = int(input())
         parents = list(map(int, input().split()))
-    elif user == "F":
+
+    elif "F" in user:
         path = './test/'
         fileName = input("File name: ")
         folder = path + fileName
+    
         if 'a' in fileName:
-                print("Please don't put letter a in file name")
-                return
-    # let user input file name to use, don't allow file names with letter a
+            print("File can not contain letter 'a' ")
+            return
         try:
-            with open(folder, 'r') as f:
-                n = int(f.readline().strip())
-                parents = list(map(int, f.readline().strip().split()))
+            with open(folder, 'r', encoding='utf-8') as file:
+                n = int(file.readline())
+                parents = list(map(int, file.readline().split()))
         except FileNotFoundError:
             print("File not found")
             return
         except ValueError:
-            print("Wrong input")
+            print("Invalid input format")
+            return
     else:
-        print("Error, Please write with I or F")
-        return
-
-    print(compute_height(n,parents))
-    return
+        print("Type 'I' or 'F': ")
+        return 
+    print(compute_height(n, parents))
 
     # account for github input inprecision
     
