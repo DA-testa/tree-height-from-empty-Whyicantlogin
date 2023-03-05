ee4b9709843c831ead53af3f4bb32b6d000f03d9
Jiaxin <114979508+Whyicantlogin@users.noreply.github.com>
2023-03-05 22:10:57 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,24 +1,64 @@
 # python3
+# Jiaxin Dai
 
 import sys
 import threading
 
-
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    # izveido listu 
+    children = [[] for _ in range(n)]
+    for child, parent in enumerate(parents):
+        if parent != -1:
+            children[parent].append(child)
+    s = parents.index(-1) # inicializēt koku sakne
+    rinda = [(s,1)]
+    height = 0 
+    while rinda:
+        n, l = rinda.pop(0)
+        if l > height:
+            height = l
+        for child in children[n]:
+            rinda.append((child, l+1))
+
+    return height
+
 
 
 def main():
     # implement input form keyboard and from files
-    
+    user = input("'I' for input, 'F' for file: ")
+    if user == "I":
+        n = int(input())
+        parents = list(map(int, input().split()))
+    elif user == "F":
+        path = './test/'
+        fileName = input("File name: ")
+        folder = path + fileName
+        if 'a' in fileName:
+                print("Please don't put letter a in file name")
+                return
     # let user input file name to use, don't allow file names with letter a
+        try:
+            with open(folder, 'r') as f:
+                n = int(f.readline().strip())
+                parents = list(map(int, f.readline().strip().split()))
+        except FileNotFoundError:
+            print("File not found")
+            return
+        except ValueError:
+            print("Wrong input")
+    else:
+        print("Error, Please write with I or F")
+        return
+
+    print(compute_height(n,parents))
+    return
+
     # account for github input inprecision
     
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
+    
     # call the function and output it's result
 
 
