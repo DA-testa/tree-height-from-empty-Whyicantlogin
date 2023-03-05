28489b5115b47a61f5084d12c43e2d76392bd189
Jiaxin <114979508+Whyicantlogin@users.noreply.github.com>
2023-03-05 23:09:09 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -3,74 +3,81 @@
 
 import sys
 import threading
+#bibliotēka
 
 def compute_height(n, parents):
-    children = [[] for _ in range(n)]
-    for child, parent in enumerate(parents):
+    children = [[] for _ in range(n)] # izveido jauna listu
+    for child, parent in enumerate(parents): 
         if parent != -1:
+        #ja parent nav -1, 
             children[parent].append(child)
+            #tad paņem child uz listu "children"
+ 
     
     def get_height(p):
         if not children[p]:
+        #ja pašlaik punktā vēl nav children, tad augstuma ir 1
             return 1
         
 
-
-
-
         else:
             return 1 + max(get_height(child) for child in children[p])
+            # ja punktā ir children, tad  rekursīvi aprēķina katra child augstumu, un tad +1
     
 
-    s = parents.index(-1)
-    return get_height(s)
+    s = parents.index(-1) # meklēt saknes punkti, kuri vēl nav parent
+    return get_height(s)  # call the function
     
 
 
 def main():
+   # implement input form keyboard and from file
     user = input("'I' for input, 'F' for file: ")
+    
+    # ja user izvēlas manuāla ievadīt datus
     if "I" in user:
-        n = int(input())
+        n = int(input()) #ņemsim punktus sk. un parent katra punktā no user
         parents = list(map(int, input().split()))
-
+    
+    #ja user izvēlas ievadīt datus no fialā
     elif "F" in user:
-        path = './test/'
+    # noteikt failu ceļu un nosaukmus uz mapē
+        path = './test/' 
         fileName = input("File name: ")
         folder = path + fileName
-    
+    # let user input file name to use, don't allow file names with letter a
         if 'a' in fileName:
             print("File can not contain letter 'a' ")
             return
         
-        
+       # meģināt atvert filu un nolasīt punktus un parents sk.  
         try:
             with open(folder, 'r', encoding='utf-8') as file:
                 n = int(file.readline())
                 parents = list(map(int, file.readline().split()))
                 
                 
-                
+      # noradīt ja failu neatrada vai nepareiz ievada         
         except FileNotFoundError:
-            print("File not found")
+            print("File not Found")
             return
         except ValueError:
-            print("Invalid input format")
+            print("Wrong input")
             return
         
         
         
-        
+     # call the function and output it's result   
     else:
         print("Type 'I' or 'F': ")
         return 
-    print(compute_height(n, parents))
+    print(compute_height(n, parents)) #izvade
 
-    # account for github input inprecision
+ 
+   
     
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
     
-    # call the function and output it's result
+    
 
 
 # In Python, the default limit on recursion depth is rather low,
